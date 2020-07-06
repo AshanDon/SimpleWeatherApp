0ebb8893d39b89da6044802e58b32a9c29eb2038
Ashan <ashan.anuruddika@icloud.com>
2020-07-06 21:13:19 +0300

    Initial Commit

---
+++ SimpleWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleWeatherApp
+//
+//  Created by Ashan Anuruddika on 7/6/20.
+//  Copyright © 2020 Ashan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
